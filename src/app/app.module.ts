06409d566b3caf324ec8769540f3b349ea8e9a4a
tibbsd1 <tibbsshuttlesworth@gmail.com>
2020-03-22 20:24:48 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
 // import { HelloComponent } from "./hello/hello.component";
@@ -10,7 +11,7 @@ import { SearchComponent } from "./search/search.component";
 import { MovieService } from "./movie.service";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule, AppRoutingModule],
   declarations: [AppComponent, SearchComponent],
   bootstrap: [AppComponent],
   providers: [MovieService]
