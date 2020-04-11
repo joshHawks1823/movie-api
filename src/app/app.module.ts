b91d2f1b54173b0a4e00fbdc6040daf5a69cb73c
joshhawks1823 <jheat23@gmail.com>
2020-04-10 20:09:31 -0400

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { MoviesService } from "./services/omdb.service.client";
-import {MatTableModule, MatTableDataSource} from '@angular/material/table'
+import { MatTableModule, MatTableDataSource } from "@angular/material/table";
 
 import { FormsModule } from "@angular/forms";
 import { TmdbTestComponent } from "./tmdb-test/tmdb-test.component";
@@ -15,46 +15,71 @@ import { FilterPipe } from "./filter.pipe";
 // import { SearchComponent } from "./search/search.component";
 // import { MovieService } from "./movie.service";
 
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SavedComponent } from './saved/saved.component';
-import { SavedService } from './saved.service';
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SavedComponent } from "./saved/saved.component";
+import { SavedService } from "./saved.service";
 
-import {MatButtonModule} from '@angular/material/button'; 
+import { MatButtonModule } from "@angular/material/button";
 
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from "@angular/material/input";
 
-import { OmdbTestComponent } from './omdb-test/omdb-test.component';
-import { MatSliderModule } from '@angular/material/slider'
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { ProfileComponent } from './profile/profile.component';
-import { AdminPortalComponent } from './admin-portal/admin-portal.component';
-import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
-import { AuthGuardService } from './services/auth-guard.service';
-import { RoleGuardService } from './services/role-guard.service';
-
+import { OmdbTestComponent } from "./omdb-test/omdb-test.component";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
+// import { MatFormFieldModule } from "@angular/material/form-field";
+import { ProfileComponent } from "./profile/profile.component";
+import { AdminPortalComponent } from "./admin-portal/admin-portal.component";
+import { JwtHelperService, JWT_OPTIONS } from "@auth0/angular-jwt";
+import { AuthGuardService } from "./services/auth-guard.service";
+import { RoleGuardService } from "./services/role-guard.service";
 
 @NgModule({
-  declarations: [   AppComponent,
+  declarations: [
+    AppComponent,
     TmdbTestComponent,
     MovieComponent,
     FilterPipe,
     SavedComponent,
     SignupComponent,
     OmdbTestComponent,
-    AppComponent, OmdbTestComponent,LoginComponent, SignupComponent, ProfileComponent, AdminPortalComponent
+    AppComponent,
+    OmdbTestComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    AdminPortalComponent,
   ],
 
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, BrowserAnimationsModule, MatButtonModule,MatFormFieldModule, MatInputModule, MatCardModule,BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, BrowserAnimationsModule, MatSliderModule, MatDividerModule, MatTableModule],
-  providers: [MoviesService, JwtHelperService, {provide:JWT_OPTIONS,useValue:JWT_OPTIONS}, AuthGuardService, RoleGuardService],
-  
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    // MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatDividerModule,
+    MatTableModule,
+  ],
+  providers: [
+    MoviesService,
+    JwtHelperService,
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    AuthGuardService,
+    RoleGuardService,
+  ],
 
-  bootstrap: [AppComponent]
-
-
-
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
